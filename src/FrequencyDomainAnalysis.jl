30d56e0a624a0f846ef93f47f6f6db356585769b
Simon Kornblith <simon@simonster.com>
2013-07-31 19:31:52 -0400

    Rename module

--- src/FrequencyDomainAnalysis.jl
+++ src/FrequencyDomainAnalysis.jl
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-module Multitaper
+module FrequencyDomainAnalysis
 using NumericExtensions
 
 export PowerSpectrum, CrossSpectrum, Coherence, Coherency, PLV, PPC, PLI, PLI2Unbiased, WPLI,
