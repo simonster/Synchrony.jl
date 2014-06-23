c27d7da75794fa3c57f2276262c4ecb0786b94c6
Simon Kornblith <simon@simonster.com>
2014-06-22 21:53:21 -0400

    License -> MIT

--- src/transform_stats.jl
+++ src/transform_stats.jl
@@ -1,22 +1,3 @@
-# FrequencyDomainAnalysis.jl
-# Tools for spectral density estimation and analysis of phase relationships
-# between sets of signals.
-
-# Copyright (C) 2013   Simon Kornblith
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 export PowerSpectrum, PowerSpectrumVariance, CrossSpectrum, Coherence, Coherency, PLV, PPC, PLI,
        PLI2Unbiased, WPLI, WPLI2Debiased, JMCircularCorrelation, JCircularCorrelation,
        ShiftPredictor, Jackknife, allpairs, applystat, permstat, jackknife_bias_var
